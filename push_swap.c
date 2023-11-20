79a25711773d8767c1a8fdb7a000725226ba948d
aweissha <aweissha@outlook.de>
2023-11-20 16:31:32 +0100

    added comments

--- push_swap.c
+++ push_swap.c
@@ -6,7 +6,7 @@
 /*   By: aweissha <aweissha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/19 16:15:22 by aweissha          #+#    #+#             */
-/*   Updated: 2023/11/20 16:18:25 by aweissha         ###   ########.fr       */
+/*   Updated: 2023/11/20 16:31:20 by aweissha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 int main (int argc, char **argv)
 {
+	// handle different command line argument inputs and make an array of number strings of it.
 	char	**array;
 	t_stack	*stack_a;
 	
@@ -21,6 +22,7 @@ int main (int argc, char **argv)
 		array = ft_split(argv[1], ' ');
 	else
 		array = &argv[1];
+	// transform the array of number strings to a stack build as a doubly linked list.
 	stack_a = ft_make_stack_a(array);
 	
 	
